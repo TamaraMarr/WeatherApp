fca279f52b2593627e0e9210df1bdc3ff7a75c5a
Tamara Mardjetko <tasha.m.bg@gmail.com>
2018-01-02 22:37:13 +0100

    API key changed

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 
 export const BASE_API_URL= "https://api.openweathermap.org/data/2.5/forecast?APPID=49aec73dd92d813a0b58b4129d67aecd&units=metric&q=";
 // export const APPID= "49aec73dd92d813a0b58b4129d67aecd";
-export const GOOGLE_API_KEY="AIzaSyB9uq4gyz_OA_zQ1GwkSw0NobbctUYv7m0";
+export const GOOGLE_API_KEY="AIzaSyD_L2AG-FJk6zfnBNed6a1MOj_3yv4kD4w";
